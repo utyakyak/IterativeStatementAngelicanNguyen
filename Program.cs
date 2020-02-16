5c7b4d0fc345fc2f77e852891b5caf1ff1856ac1
Angelica Nguyen <Angelica@LAPTOP-C8KSJC6A>
2020-02-16 16:05:24 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace IterativeStatementAngelicanNguyen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter an integer between 2 and 10");
+            int UserNumber;
+            while (int.TryParse(Console.ReadLine(), out UserNumber))
+            {
+                if (UserNumber >= 2 && UserNumber <= 10)
+                {
+                    if(UserNumber %2 ==1)
+                    {
+                        Console.WriteLine("You entered an odd number");
+                    }
+                    else
+                    {
+                        Console.WriteLine("You entered an even number");
+                    }
+                }
+                else
+                {
+                    Console.Clear();
+                    Console.WriteLine("Enter an integer between 2 and 10");
+                }
+            }
+        }
+    }
+}
+           
